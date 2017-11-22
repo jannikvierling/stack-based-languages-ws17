6ae1220f5c2e35b5298b801a39095eed7fa823a6
th-b <thomas.baumhauer@gmail.com>
2017-11-22 19:30:37 +0100

    Some examples

---
+++ test_clauses.fs
@@ -0,0 +1,35 @@
+1 2 3
+0
+3 times insert-literal
+constant c0
+
+1 -2 4
+0
+3 times insert-literal
+constant c1
+
+-1 -2 4
+0
+3 times insert-literal
+constant c2
+
+c1 c0 0 insert-clause insert-clause constant s0
+
+1 2
+0
+2 times insert-literal
+constant d0
+
+-1 2
+0
+2 times insert-literal
+constant d1
+
+-2
+0
+insert-literal
+constant d2
+
+d2 d1 d0
+0
+3 times insert-clause constant t0
