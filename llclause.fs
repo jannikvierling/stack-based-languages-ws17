6d40736f58e7ed36662cd888a24d1607686366f5
th-b <thomas.baumhauer@gmail.com>
2017-11-22 16:03:38 +0100

    added a linear time clause merge
    utilizing the fact that clauses are ordered

--- llclause.fs
+++ llclause.fs
@@ -3,6 +3,22 @@ struct
     cell% field clause-literal
 end-struct clause%
 
+: literal-equal { l1 l2 -- flag } l1 l2 = ;
+
+: literal-negated { l1 l2 -- flag } l1 l2 negate = ;
+
+: literal-less { l1 l2 -- flag }
+	l1 abs l2 abs <
+		l1 abs l2 abs =
+		l1 l2 <
+		and
+	or
+	;
+
+: literal-greater { l1 l2 -- flag }
+	l1 l2 literal-less l1 l2 literal-equal or invert
+	;
+
 : clause-size ( clause -- n ) recursive
     { clause }  clause 0= IF
         0
@@ -37,11 +53,11 @@ end-struct clause%
         clause% %allot
         0 over clause-next !
         literal over clause-literal !
-    ELSE clause clause-literal @ literal > IF
+    ELSE clause clause-literal @ literal literal-greater IF
             clause% %allot dup dup
             clause swap clause-next !
             literal swap clause-literal !
-        ELSE clause clause-literal @ literal < IF
+        ELSE clause clause-literal @ literal literal-less IF
                 literal clause clause-next @ insert-literal
                 clause clause-next !
                 clause
@@ -71,13 +87,56 @@ end-struct clause%
         
 : merge-clauses ( clause1 clause2 -- clause )
     { clause1 clause2 } clause1 copy-clause clause2 merge-clauses' ;
-            
+		
+: fast-merge' recursive { result clause1 clause2 -- } 
+	clause1 0= IF
+		clause2 copy-clause result clause-next !
+	ELSE
+		clause2 0= IF
+			clause1 copy-clause result clause-next !
+		ELSE
+			\ Almost identical parts of code repeated three times
+			\ Could be a lot shorter with smarter usage of IF but
+			\ also a lot less readable
+			clause1 clause-literal @ clause2 clause-literal @ literal-equal IF
+				clause% %allot
+				0 over clause-next !	
+				clause1 clause-literal @ over clause-literal !
+				dup result clause-next !
+				clause1 clause-next @ clause2 clause-next @ fast-merge'
+			ELSE
+				clause1 clause-literal @ clause2 clause-literal @ literal-less IF
+					clause% %allot
+					0 over clause-next !	
+					clause1 clause-literal @ over clause-literal !
+					dup result clause-next !
+					clause1 clause-next @ clause2 fast-merge'
+				ELSE
+					clause% %allot
+					0 over clause-next !	
+					clause2 clause-literal @ over clause-literal !
+					dup result clause-next !
+					clause1 clause2 clause-next @ fast-merge'
+				ENDIF
+			ENDIF
+		ENDIF
+	ENDIF ;
+
+\ Create a dummy first element to work with, then drop it
+: fast-merge { clause1 clause2 -- clause }
+	clause% %allot
+	0 over clause-next !
+	0 over clause-literal !
+	dup	clause1 clause2 fast-merge'
+	clause-next @ ;
+	
 \ Todo: Free the copies.
+\ Question: Doesn't gforth have some sort of garbage collection?
 : resolve-clauses ( literal clause1 clause2 -- resolvent )
     { literal clause1 clause2 }
     literal clause1 copy-clause remove-literal
     literal negate clause2 copy-clause remove-literal
-    merge-clauses ;
+    fast-merge ;
 
 : clauses-equal ( clause1 clause2 -- x ) recursive
     \ Compares two clauses for equality.
@@ -108,7 +167,6 @@ end-struct clause%
             clause-next @ contains-literal
         ENDIF
     ENDIF ;
-
 
 \ todo: Is is possible to simplify this word by computing the
 \ resolvable atoms first?.
