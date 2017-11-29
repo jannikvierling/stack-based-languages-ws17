6366bf66b67e28f79c9defe41536fff13169bded
Jannik Vierling <jannik.vierling@gmail.com>
2017-11-29 23:07:06 +0100

    add iterative definition for list length

--- list.fs
+++ list.fs
@@ -7,14 +7,10 @@ end-struct list%
                 drop true exit
             ENDIF list-next @ REPEAT drop false ;
 
-: list-length ( list -- n ) recursive
-    \ "list" is a pointer to the first element of a linked list
-    \ "n" is the length of the list.
-    { list } list 0= IF
-        0
-    ELSE
-        list list-next @ list-length 1+
-    ENDIF ;
+: list-length ( list -- n)
+    0 swap BEGIN
+        dup 0<> WHILE list-next @ swap 1+ swap
+    REPEAT drop ;
 
 : list-pop ( list1 -- list1 list2 )
     dup list-next @ ;
