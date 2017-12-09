4e5f599da7665bfdb4b7d46e01b7da1ca8da1626
Jannik Vierling <jannik.vierling@gmail.com>
2017-12-09 10:55:41 +0100

    added a more complex example

--- prover_test.fs
+++ prover_test.fs
@@ -2,22 +2,37 @@ require clause.fs
 require prover.fs
 require util.fs
 
--1 0 insert-literal
-dup is-not-seen .
+variable trivial
 
--2 1 3 0 3 times insert-literal
-dup is-not-seen .
-
-2 0 insert-literal
-dup is-not-seen .
-
--3 0 insert-literal
-dup is-not-seen .
-
-\ Irrelevant clause (first four clauses are unsat)
+-1          0         insert-literal
+-2 1 3      0 3 times insert-literal
+2           0         insert-literal
+-3          0         insert-literal
 -1 -2 2 3 1 0 5 times insert-literal
 
-0 5 times append-if-new
+0 5 times append-if-new trivial !
 
-\ status 
-main
+\ trivial @ main
+
+\ Pigeon hole principle: 3 pigeons, 2 holes (UNSAT)
+
+variable php_3^2
+
+1 4 0 2 times insert-literal
+2 5 0 2 times insert-literal
+3 6 0 2 times insert-literal
+
+-1 -4 0 2 times insert-literal
+-2 -5 0 2 times insert-literal
+-3 -6 0 2 times insert-literal
+
+-1 -2 0 2 times insert-literal
+-1 -3 0 2 times insert-literal
+-2 -3 0 2 times insert-literal
+-4 -5 0 2 times insert-literal
+-4 -6 0 2 times insert-literal
+-5 -6 0 2 times insert-literal
+
+0 12 times append-if-new php_3^2 !
+
+\ php_3^2 @ main
