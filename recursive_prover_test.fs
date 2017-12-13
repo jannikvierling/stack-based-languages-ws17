04e6a0e05bee0cb435565a8bf272156f2603ae3a
th-b <thomas.baumhauer@gmail.com>
2017-12-13 18:27:38 +0100

    added examples for presentation

--- recursive_prover_test.fs
+++ recursive_prover_test.fs
@@ -3,18 +3,46 @@ require literal.fs
 require clause.fs
 require clauseset.fs
 require recursive_prover.fs
+require parser.fs
 
-variable example
 
-1 2 3 0   3 times insert-literal
-1 -2 4 0  3 times insert-literal
--1 -2 4 0 3 times insert-literal
-1 2 0     2 times insert-literal
--1 2 0    2 times insert-literal
--2 0              insert-literal
+begin-dimacs
 
-0         6 times insert-clause
+1 2			0
+-1 2		0
+-2 			0
 
-example !
+3 clauses
 
-\ is-sat invert .
+end-dimacs 
+constant very-simple
+
+\ verbose
+\ very-simple refute
+
+\ ----------------------------------------------
+
+begin-dimacs
+
+1 2			0
+-1 2		0
+2 3			0
+
+3 clauses
+
+end-dimacs 
+constant very-simple2
+
+\ very-simple2 refute
+
+\ ----------------------------------------------
+
+1 2 3 0		3 times insert-literal constant c0
+-1 -2 4 0	3 times insert-literal constant c1
+
+\ c0 c1 resolve-all
+\ make-set-from-stack
+
+\ Compare Haskell:
+\ fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+\ take 5 fibs
