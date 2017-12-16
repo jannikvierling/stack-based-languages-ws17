6614799362dc2d1560866c3fac94550ee175c822
Jannik Vierling <jannik.vierling@gmail.com>
2017-12-16 13:30:52 +0100

    implement append-if-new iteratively

--- clauselist.fs
+++ clauselist.fs
@@ -14,19 +14,17 @@ end-struct clauselist%
     dup clause swap clauselist-clause !
     dup 0 swap list-next ! ;
 
-: append-if-new recursive { clause clauselist -- clauselist }
-    clauselist 0= IF
-        clause 0 new-clause-list-node
-    ELSE
-        clauselist clauselist-clause @ clause clauses-equal IF
-            clauselist
-        ELSE
-            clause clauselist list-next @ append-if-new
-            clauselist list-next !
-            clauselist
-        ENDIF
-    ENDIF ;
-
+: append-if-new ( clause clauselist -- clauselist )
+    dup 0= GUARD
+        drop 0 new-clause-list-node END
+    tuck 0 BEGIN
+            drop
+            over over clauselist-clause @ clauses-equal IF
+                2drop EXIT
+            ENDIF dup list-next @ swap
+        over 0=
+        UNTIL nip swap 0 new-clause-list-node swap list-next ! ;
+    
 : contains-clause ( clause* list<clause*> -- f )
     { clause list } ['] clauselist-clause ['] clauses-equal clause list list-search ;
 
