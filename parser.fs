31d69eef8a5e46aa9f8920ab179669a7df295aff
Jannik Vierling <jannik.vierling@gmail.com>
2017-12-19 21:01:15 +0100

    minor changes to dimacs parser

--- parser.fs
+++ parser.fs
@@ -5,13 +5,12 @@ require clauselist.fs
 : begin-dimacs ( -- 0 ) 0 ;
 
 : parse-clause ( 0 l1 ... ln -- clause )
-    0 BEGIN
-        swap dup 0<> WHILE
+    0 BEGIN swap dup 0<> WHILE
             swap insert-literal
     REPEAT drop ;
 
 : end-dimacs ( dimacs clauses -- clauselist )
-    0 swap 0 u+do >r parse-clause r> append-if-new loop ;
+    0 swap 0 u+do >r parse-clause r> insert-clause loop ;
 
-: clauses ( .. 0 clauses -- ... clauses ) swap drop ;
+: clauses ( .. 0 clauses -- ... clauses ) nip ;
 
